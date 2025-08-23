f62c8622c16aabb1530f433f44b6b3e549fd4561
VirtuonBeta <shahid.id001@gmail.com>
2025-08-23 16:57:23 +0200

    Update rate_limiter.py

--- module-1/rate_limiter.py
+++ module-1/rate_limiter.py
@@ -8,7 +8,7 @@ class RateLimiter:
         self.config = config
         self.request_timestamps: List[float] = []
         self.max_requests = int(config.max_requests_per_minute * config.safety_margin)
-        self.time_window = 60  # 1 minute in seconds
+        self.time_window = 60  # 1-minute window as per Binance rate limits
     
     def wait_if_needed(self):
         """Check if we're approaching rate limit and wait if necessary"""
@@ -21,8 +21,9 @@ class RateLimiter:
             sleep_time = self.time_window - (now - self.request_timestamps[0])
             if sleep_time > 0:
                 time.sleep(sleep_time)
-                # After waiting, clear old timestamps
-                self.request_timestamps = []
+                # After waiting, remove expired timestamps instead of clearing all
+                now = time.time()
+                self.request_timestamps = [t for t in self.request_timestamps if now - t < self.time_window]
         
         # Record this request
         self.request_timestamps.append(now)
